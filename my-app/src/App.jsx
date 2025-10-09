facc31b92b2aece8b025e069cc75f275f3e18aca
hanshika0013 <hanshika0072@gmail.com>
2025-10-09 15:11:51 +0530

    update all

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import Banner from './Pages/Banner'
-import Video from './Pages/Video'
-import Counter from './Pages/Counter'
-import Misson from './Pages/Misson'
-import Journey from './Pages/Journey'
-import Form from './Pages/Form'
-import Experience from './Pages/Experience'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,12 +8,6 @@ function App() {
   return (
     <>
    <Banner/>
-   <Video/>
-   <Counter/>
-   <Misson/>
-   <Journey/>
-   <Form/>
-   <Experience/>
     </>
   )
 }
